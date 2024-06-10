ed1203171937a580244e56f9e6c11ca63a9bf2ac
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-06-10 18:50:09 +0300

    Add Dockerfile for Flask and modified Flask App

--- Application/app.py
+++ Application/app.py
@@ -66,7 +66,8 @@ def login():
     
     return render_template('login.html')
 
+# Run Application in port 80
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
