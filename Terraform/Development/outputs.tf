249f49943c81113fc51751ccde02ba6a20466e0f
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-06-11 22:56:21 +0300

    Add Development tf files

--- Terraform/Development/outputs.tf
+++ Terraform/Development/outputs.tf
@@ -2,3 +2,26 @@
 #FlaskPipeline Project
 #Created by Matvey Guralskiy
 #---------------------------
+
+output "VPC_ID" {
+  value       = aws_vpc.VPC_FlaskPipeline.id
+  description = "My VPC ID"
+}
+
+output "VPC_cidr" {
+  value       = aws_vpc.VPC_FlaskPipeline.cidr_block
+  description = "My VPC CIDR Block"
+}
+
+output "Public_Subnet_ID" {
+  value       = aws_subnet.Public_Subnet.id
+  description = "Public Subnets ID of VPC"
+}
+
+output "Master_Instance_Public_IP" {
+  value = aws_instance.Master_Instance.public_ip
+}
+
+output "Database_Public_IP" {
+  value = aws_instance.Postgres_Database.public_ip
+}
