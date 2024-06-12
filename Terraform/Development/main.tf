684754ada87eaffe2ec3e0dfbd1ee927bc28d89a
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-06-12 12:08:29 +0300

    Change Dockerfile and Bash script and tf files

--- Terraform/Development/main.tf
+++ Terraform/Development/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 # Remote State sends on S3 Bucket
 terraform {
   backend "s3" {
-    bucket  = "flaskpipeline-project-bucket"
+    bucket  = "flaskpipeline-project-development"
     key     = "Development/terraform.tfstate"
     region  = "eu-central-1"
     encrypt = true
