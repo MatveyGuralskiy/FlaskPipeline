9598c76fe9b390204464734e4d78f7e4777cb2ce
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-06-12 10:20:14 +0300

    Change Terraform files and Application Debug

--- Application/app.py
+++ Application/app.py
@@ -71,4 +71,4 @@ def login():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=80)
