892fc1aef40cea2f71b027002c3731522f804b7e
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-06-12 16:13:25 +0300

    Modified Terraform Build files and Bash script

--- Terraform/Build/variables.tf
+++ Terraform/Build/variables.tf
@@ -3,20 +3,38 @@
 #Created by Matvey Guralskiy
 #---------------------------
 
-variable "Region" {
-  description = "AWS Region to work with"
+variable "Region_GER" {
+  description = "AWS Region to work with Germany"
   type        = string
   default     = "eu-central-1"
 }
 
-variable "Environment" {
-  description = "Environment for the Project"
+variable "Region_USA" {
+  description = "AWS Region to work with USA"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "Env_Development" {
+  description = "Environment for the Project Development"
   type        = string
   default     = "Development"
 }
 
-variable "Remote_State_S3" {
+variable "Env_Production" {
+  description = "Environment for the Project Production"
+  type        = string
+  default     = "Production"
+}
+
+variable "Remote_State_S3_Dev" {
   description = "Terraform Backend Bucket Name"
   type        = string
   default     = "flaskpipeline-project-development"
+}
+
+variable "Remote_State_S3_Prod" {
+  description = "Terraform Backend Bucket Name for Production"
+  type        = string
+  default     = "flaskpipeline-project-production"
 }
