43429ab6bfeabb5178d1eeb6be490c26a1c9932e
Matvey Guralskiy <mathewguralskiy@gmail.com>
2024-06-23 21:05:13 +0300

    Modified Ansible, Terraform and Bash Scripts

--- Bash/master_ansible.sh
+++ Bash/master_ansible.sh
@@ -3,7 +3,7 @@
 #FlaskPipeline Project
 #Created by Matvey Guralskiy
 #---------------------------
-
+sudo apt update
 # Install Ansible
 sudo apt-get install -y ansible
 
@@ -15,3 +15,9 @@ sudo apt install python3-boto3
 
 # Clone GitHub Repository
 git clone https://github.com/MatveyGuralskiy/FlaskPipeline.git
+
+# Install AWS CLI
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
