d963729ace2fc5186a56d78a6f8a866f6ecd93a2
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-06-26 01:16:22 +0300

    Add new version of Application

--- Bash/master_instance.sh
+++ Bash/master_instance.sh
@@ -80,3 +80,8 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-k
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update -y
 sudo apt-get install trivy -y
+
+# Upgrade for Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
