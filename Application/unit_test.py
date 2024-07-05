602ce4dbb9e28e0e2dc14f1b38a584be789d7487
Matvey Guralskiy <mathewguralskiy@gmail.com>
2024-07-05 12:09:32 +0300

    Modified Unittest file

--- Application/unit_test.py
+++ Application/unit_test.py
@@ -1,10 +1,14 @@
-import unittest
-from app import app
+#---------------------------
+#FlaskPipeline Project
+#Created by Matvey Guralskiy
+#---------------------------
+import unittest                                           # Unit test module
+from app import app                                       # Work with Application file
 
 class TestApp(unittest.TestCase):
     
-    def setUp(self):
-        self.app = app.test_client()
+    def setUp(self):                                      # Sets up a test environment
+        self.app = app.test_client()                      # Creates a test client to send requests
 
     # Check the Main Page    
     def test_index(self):
@@ -24,5 +28,5 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<h1>Sign Up</h1>', response.data)
 
-if __name__ == '__main__':
-    unittest.main(testRunner=unittest.TextTestRunner())
+if __name__ == '__main__':                                # Not the main Application
+    unittest.main(testRunner=unittest.TextTestRunner())   # Initiates the execution of unit test
