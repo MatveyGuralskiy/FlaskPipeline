db0b3bd907e4fa2723d971d4a4fef4907bd656ac
Matvey Guralskiy <mathewguralskiy@gmail.com>
2024-07-05 10:58:29 +0300

    Add descriptions to JavaScript file

--- Application/static/js/scripts.js
+++ Application/static/js/scripts.js
@@ -1,21 +1,21 @@
 /* JavaScript file for Website */
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { //waits for HTML to be downloaded
     // Attach class fade in to every content
-    const content = document.querySelector('.content');
+    const content = document.querySelector('.content'); //finds the first element on the page with content class
     if (content) {
         content.classList.add('fade-in');
     }
 
     // Fade in between pages
-    const navLinks = document.querySelectorAll('nav ul li a');
+    const navLinks = document.querySelectorAll('nav ul li a'); //finds all links
     navLinks.forEach(link => {
         link.addEventListener('click', (event) => {
             event.preventDefault();
-            const targetUrl = link.getAttribute('href');
+            const targetUrl = link.getAttribute('href'); //takes link from href
             content.classList.remove('fade-in');
             content.classList.add('fade-out');
             setTimeout(() => {
-                window.location.href = targetUrl;
+                window.location.href = targetUrl; //timeout of 0.5 sec
             }, 500);
         });
     });
