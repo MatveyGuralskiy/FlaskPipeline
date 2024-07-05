0a8d820da5948c58c539537a6a57e67968357e95
Matvey Guralskiy <mathewguralskiy@gmail.com>
2024-07-05 11:59:11 +0300

    Modified Models descriptions

--- Application/models.py
+++ Application/models.py
@@ -1,13 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+#---------------------------
+#FlaskPipeline Project
+#Created by Matvey Guralskiy
+#---------------------------
+from flask_sqlalchemy import SQLAlchemy                                # SQLAlchemy allows to interact with databases
+from flask_bcrypt import Bcrypt                                        # Bcrypt for password hashing
 
-db = SQLAlchemy()
-bcrypt = Bcrypt()
+db = SQLAlchemy()                                                      # Initialize SQLAlchemy object for database operations
+bcrypt = Bcrypt()                                                      # Initialize Bcrypt object for password encryption
 
 # User Table
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    salt = db.Column(db.String(32), nullable=False)
+    # Define columns for the User table
+    id = db.Column(db.Integer, primary_key=True)                       # Primary key
+    username = db.Column(db.String(150), unique=True, nullable=False)  # Username, unique and mandatory field
+    email = db.Column(db.String(150), unique=True, nullable=False)     # Email, unique and mandatory field
+    password = db.Column(db.String(200), nullable=False)               # Hashed password, mandatory field
+    salt = db.Column(db.String(32), nullable=False)                    # Salt for password uniqueness
